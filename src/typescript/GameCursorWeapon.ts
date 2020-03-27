96397dcbce76b8e269eb6e1b6caf555f85832900
Patrick Obermueller <patrick.obermueller@willhaben.at>
2020-03-27 21:39:24 +0100

    add weaponeffect animation

--- src/typescript/GameCursorWeapon.ts
+++ src/typescript/GameCursorWeapon.ts
@@ -21,6 +21,12 @@ export default class GameCursorWeapon {
 	private _context: CanvasRenderingContext2D;
 
 	private _gameWeaponEffect: GameWeaponEffect[] = new Array() as GameWeaponEffect[];
+	private _rotateSpeed: number = 22.5;
+	private _rotateDegreeThreshold: number = 45;
+
+	public angle: number = 0;
+	public moveForward: boolean = true;
+	public animateHammer: boolean = false;
 
 	constructor(context: CanvasRenderingContext2D, gameCursor: GameCursor) {
 		this._gameCursor = gameCursor;
@@ -56,31 +62,67 @@ export default class GameCursorWeapon {
 		this.drawGameCursorWeapon();
 	}
 
+	public degToRad(degree: number) {
+		return degree * 0.01745;
+	}
+
 	private drawGameCursorWeapon() {
-		const { _cursorImage, _gameCursor } = this;
-		const canvasX =
-			_gameCursor.mousePosition.x -
-			_cursorImage!.img.width / _cursorImage!.scaleOnCanvas / 2;
-		const canvasY =
-			_gameCursor.mousePosition.y -
-			_cursorImage!.img.height / _cursorImage!.scaleOnCanvas / 2;
+		const { _cursorImage, _context } = this;
+
+		const sourceImage = _cursorImage!.img;
+		const sourceImageWidth = _cursorImage!.img.width;
+		const sourceImageHeight = _cursorImage!.img.height;
 
 		const scaledWidth =
 			_cursorImage!.img.width / _cursorImage!.scaleOnCanvas;
 		const scaledHeight =
 			_cursorImage!.img.height / _cursorImage!.scaleOnCanvas;
 
+		this._context.save();
+		this.setRotationPivotToMouse('set');
+
+		_context.rotate(this.degToRad(-this.angle));
+
 		this._context.drawImage(
-			_cursorImage!.img,
+			sourceImage,
 			0,
 			0,
-			_cursorImage!.img.width,
-			_cursorImage!.img.height,
-			canvasX,
-			canvasY,
+			sourceImageWidth,
+			sourceImageHeight,
+			-scaledWidth / 2,
+			-scaledHeight,
 			scaledWidth,
 			scaledHeight
 		);
+
+		this.setRotationPivotToMouse('reset');
+		_context.restore();
+
+		this.updateHammerRotation();
+	}
+
+	private setRotationPivotToMouse(rotationDirection: 'set' | 'reset') {
+		const directionNumber = rotationDirection === 'set' ? 1 : -1;
+
+		this._context.translate(
+			directionNumber * this._gameCursor.mousePosition.x,
+			directionNumber * this._gameCursor.mousePosition.y
+		);
+	}
+
+	private updateHammerRotation() {
+		if (this.animateHammer) {
+			if (this.angle <= this._rotateDegreeThreshold && this.moveForward) {
+				this.angle += this._rotateSpeed;
+			} else {
+				this.moveForward = false;
+				if (this.angle <= 0) {
+					this.moveForward = true;
+					this.animateHammer = false;
+				}
+				this.angle -= this._rotateSpeed;
+			}
+		}
 	}
 
 	private removeActiveEffect() {
@@ -90,6 +132,7 @@ export default class GameCursorWeapon {
 	private addEventListeners() {
 		document.addEventListener('click', (event: MouseEvent) => {
 			GameAudio.playSoundOverlap(this._currentAudio!.audio);
+			this.animateHammer = true;
 
 			const newGameWeaponEffect = new GameWeaponEffect(
 				this._context,

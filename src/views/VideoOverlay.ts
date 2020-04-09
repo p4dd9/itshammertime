cb6f7ac64e81d1c980b51cf59b6c054234afbffb
Patrick Obermueller <patrick.obermueller@willhaben.at>
2020-04-09 18:29:18 +0200

    add layers div container and create more canvas layers

--- src/views/VideoOverlay.ts
+++ src/views/VideoOverlay.ts
@@ -4,22 +4,29 @@ import Game from '../typescript/Game';
 window.onload = (): void => {
 	const bodyMarginVerticalHorizontal = 16;
 
-	const root: HTMLElement | null = document.getElementById('root');
+	const root: HTMLElement | null = document.getElementById('game-layer');
+	if (root === null) {
+		return;
+	}
 
 	const height = window.innerHeight - bodyMarginVerticalHorizontal;
 	const width = window.innerWidth - bodyMarginVerticalHorizontal;
 
-	const canvasDOMElement = document.createElement('canvas');
-	canvasDOMElement.width = width;
-	canvasDOMElement.height = height;
+	const layers = [];
 
-	root!.appendChild(canvasDOMElement);
+	for (let i = 0; i < 3; i++) {
+		const canvasDOMElement = document.createElement('canvas');
+		canvasDOMElement.width = width;
+		canvasDOMElement.height = height;
+		canvasDOMElement.id = `layer-${i}`;
+		canvasDOMElement.style.position = 'absolute';
+		const domElementAppened = root.appendChild(canvasDOMElement);
+		layers.push(
+			domElementAppened.getContext('2d') as CanvasRenderingContext2D
+		);
+	}
 
-	const context: CanvasRenderingContext2D | null = canvasDOMElement.getContext(
-		'2d'
-	);
-
-	const game: Game = new Game(context!);
+	const game: Game = new Game(layers[0]);
 	game.start();
 
 	window.onresize = (): void => {

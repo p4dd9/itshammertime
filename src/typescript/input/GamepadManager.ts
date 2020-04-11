6924df4db2210a4d9979b72b83d57a8768ca1d1a
Patrick Obermueller <patrick.obermueller@willhaben.at>
2020-04-11 19:59:40 +0200

    refactor gamepadmanager and remove unused logic

--- src/typescript/input/GamepadManager.ts
+++ src/typescript/input/GamepadManager.ts
@@ -7,21 +7,11 @@ import Weapon from '../Weapon';
 import Input from '../../interfaces/Input';
 
 export default class GamepadManager implements Input {
-	private throttleTimerId: undefined | number = undefined;
-	private buttonReadInterval = 90; // time between button input locked to not ready any input
+	private gamepad: Gamepad | null = null;
 
-	private _gamepad: Gamepad | null = null;
+	private throttleId: undefined | number = undefined;
+	private delay = 95;
 
-	// State of the Controll Joystick Axes
-	private axesStatus = new Array(4) as number[];
-
-	// State of buttons (TODO: This should be savely removeable)
-	private _buttonsStatus = [] as string[];
-
-	// XBOX 360 MAPPING
-	private buttons = ['A', 'B', 'X', 'Y'];
-
-	private intervalId = -1;
 	private axeStatusThreshold = 0.3;
 	private gameInput: Controller;
 	private weapon: Weapon;
@@ -34,21 +24,8 @@ export default class GamepadManager implements Input {
 		this.handleGamepadDisconnected = this.handleGamepadDisconnected.bind(
 			this
 		);
-		this.listenToGamepad = this.listenToGamepad.bind(this);
 
 		this.start();
-	}
-
-	public get buttonStatus(): string[] {
-		return this._buttonsStatus;
-	}
-
-	public set buttonStatus(buttonStatus: string[]) {
-		this._buttonsStatus = buttonStatus;
-	}
-
-	public get gamepad(): Gamepad | null {
-		return this._gamepad;
 	}
 
 	public start(): void {
@@ -66,105 +43,62 @@ export default class GamepadManager implements Input {
 	}
 
 	public handleInput(): void {
+		this.gamepad = navigator.getGamepads()[0];
 		this.handleButtons();
 		this.handleAxesInput();
 	}
 
-	private listenToGamepad(): void {
-		if (this._gamepad === null) {
-			return;
-		}
-		const { buttons, axes } = this._gamepad;
-
-		// Reset everything
-		this._buttonsStatus = [];
-		const pressed = [];
-
-		// Buttons
-		if (buttons) {
-			for (
-				let buttonIndex = 0, buttnCount = buttons.length;
-				buttonIndex < buttnCount;
-				buttonIndex++
-			) {
-				if (buttons[buttonIndex].pressed) {
-					pressed.push(this.buttons[buttonIndex]);
-				}
-			}
-		}
-
-		// Axes
-		const newAxes = [];
-		if (axes) {
-			for (let a = 0, x = axes.length; a < x; a++) {
-				newAxes.push(axes[a].toFixed(2));
-			}
-		}
-
-		this.axesStatus = newAxes.map(Number);
-		this._buttonsStatus = pressed;
-		this._gamepad = navigator.getGamepads()[0];
-	}
-
-	private throttleFunction = (callback: () => void, delay: number): void => {
-		if (this.throttleTimerId) {
-			return;
-		}
-
-		this.throttleTimerId = window.setTimeout(() => {
-			callback();
-			this.throttleTimerId = undefined;
-		}, delay);
-	};
-
 	public handleAxesInput(): void {
-		if (this.axesStatus[XBOX360_AXIS.LS_X] > this.axeStatusThreshold) {
+		const axes = this.mapGamepadAxes();
+		if (axes[XBOX360_AXIS.LS_X] > this.axeStatusThreshold) {
 			this.weapon.moveRight();
 		}
-		if (this.axesStatus[XBOX360_AXIS.LS_X] < -this.axeStatusThreshold) {
+		if (axes[XBOX360_AXIS.LS_X] < -this.axeStatusThreshold) {
 			this.weapon.moveLeft();
 		}
-		if (this.axesStatus[XBOX360_AXIS.LS_Y] < -this.axeStatusThreshold) {
+		if (axes[XBOX360_AXIS.LS_Y] < -this.axeStatusThreshold) {
 			this.weapon.moveUp();
 		}
-		if (this.axesStatus[XBOX360_AXIS.LS_Y] > this.axeStatusThreshold) {
-			this. weapon.moveDown();
+		if (axes[XBOX360_AXIS.LS_Y] > this.axeStatusThreshold) {
+			this.weapon.moveDown();
 		}
 	}
 
 	public handleButtons(): void {
-		if (this.gamepad === null || this.gamepad === undefined) {
+		if (this.gamepad?.buttons[XBOX360_BUTTONS.A].pressed) {
+			this.throttle(() => {
+				this.weapon.use();
+			}, this.delay);
+		}
+	}
+
+	private mapGamepadAxes(): number[] {
+		const gamepadAxes = [];
+		for (let i = 0; i < (this.gamepad?.axes.length ?? 0); i++) {
+			gamepadAxes.push(Number(this.gamepad?.axes[i].toFixed(2)));
+		}
+		return gamepadAxes;
+	}
+
+	private throttle = (callback: () => void, delay: number): void => {
+		if (this.throttleId) {
 			return;
 		}
 
-		if (this.gamepad.buttons[XBOX360_BUTTONS.A].pressed) {
-			this.throttleFunction(() => {
-				this.weapon.use();
-			}, this.buttonReadInterval);
-		}
-	}
+		this.throttleId = window.setTimeout(() => {
+			callback();
+			this.throttleId = undefined;
+		}, delay);
+	};
 
 	private handleGamepadConnected(event: Event): void {
-		const gampadEvent = event as IGamepadEvent;
 		this.gameInput.controls = CONTROLS.GAMEPAD;
-
-		this.intervalId = window.setInterval(this.listenToGamepad, 100);
-
-		this._gamepad = gampadEvent.gamepad;
-		const { index, id, buttons, axes } = gampadEvent.gamepad;
-		console.info(
-			`Gamepad connected at index ${index}: ${id}. ${buttons.length} buttons, ${axes.length} axes.`
-		);
+		this.gamepad = (event as IGamepadEvent).gamepad;
 	}
 
-	private handleGamepadDisconnected(event: Event): void {
-		const gampadEvent = event as IGamepadEvent;
-		clearInterval(this.intervalId);
-
+	private handleGamepadDisconnected(): void {
 		this.gameInput.controls = CONTROLS.MOUSE;
-		delete this._gamepad;
-		const { index, id } = gampadEvent.gamepad;
-		console.info(`Gamepad disconnected from index ${index}: ${id}. `);
+		delete this.gamepad;
 	}
 
 	private addGamepadConnectListener(): void {

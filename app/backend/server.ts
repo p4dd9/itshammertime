609f76388c6007a2911e083478f585a4b9e41779
Patrick Obermueller <patrick.obermueller@willhaben.at>
2020-05-30 00:26:53 +0200

    add mongo db

--- app/backend/server.ts
+++ app/backend/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import mongodb from 'mongodb';
+import bodyParser from 'body-parser';
+
+const mongoClient = mongodb.MongoClient;
 
 const server = express();
+server.use(bodyParser.json());
 
 server.listen(3535, function () {
 	console.log('listening on 3535');
@@ -9,3 +14,26 @@ server.listen(3535, function () {
 server.get('/', (req, res) => {
 	res.send('Hello World!!!!!!!');
 });
+
+const mongoClientConnectionString = 'mongodb://localhost:27017';
+
+mongoClient
+	.connect(mongoClientConnectionString, {
+		useUnifiedTopology: true,
+	})
+	.then((client) => {
+		const db = client.db('itshammertime');
+		const usersCollection = db.collection('users');
+
+		server.post('/users', (req, res) => {
+			console.log(req.body);
+
+			usersCollection
+				.insertOne(req.body)
+				.then(() => {
+					res.send({ message: 'nice' });
+				})
+				.catch(() => res.send({ message: 'shit' }));
+		});
+	})
+	.catch(console.error);

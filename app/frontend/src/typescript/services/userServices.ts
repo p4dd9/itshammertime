84cc7f737aa2c87b8d017f0aea45f9c5386e0e4a
Patrick Obermueller <patrick.obermueller@willhaben.at>
2020-07-22 20:38:48 +0200

    format userServices

--- app/frontend/src/typescript/services/userServices.ts
+++ app/frontend/src/typescript/services/userServices.ts
@@ -1,9 +1,7 @@
 import { TransactionObject } from '../../types/twitch';
 import UserDTO from '../../../../backend/src/dto/UserDTO';
 
-export const sendBits = async (
-	transactionObject: TransactionObject
-): Promise<void> => {
+export const sendBits = async (transactionObject: TransactionObject): Promise<void> => {
 	try {
 		const body = JSON.stringify({
 			id: String(transactionObject.userId),
@@ -27,16 +25,13 @@ export const loadUserData = async (
 	token: string
 ): Promise<UserDTO | null | undefined> => {
 	try {
-		const response = await window.fetch(
-			`https://localhost:3535/user/${id}`,
-			{
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: 'Bearer' + token,
-				},
-			}
-		);
+		const response = await window.fetch(`https://localhost:3535/user/${id}`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer' + token,
+			},
+		});
 
 		const user = await response.json();
 		return user;

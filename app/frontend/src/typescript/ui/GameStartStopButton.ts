edb31985daaacf44ffff905003ba4ff8e6e5418d
Patrick Obermueller <patrick.obermueller@willhaben.at>
2020-10-11 01:56:19 +0200

    add hide show eye icon on bottom left

--- app/frontend/src/typescript/ui/GameStartStopButton.ts
+++ app/frontend/src/typescript/ui/GameStartStopButton.ts
@@ -1,9 +1,15 @@
 import Game from '../Game';
+import EyeOff from '../../assets/images/eye-off-outline.svg';
+import EyeOn from '../../assets/images/eye-outline.svg';
 
 export default class GameStartStopButton {
 	private button: HTMLElement = document.getElementById(
 		'ui-activate-button'
 	) as HTMLButtonElement;
+
+	private buttonImage: HTMLImageElement = document.getElementById(
+		'ui-activate-button-image'
+	) as HTMLImageElement;
 	private game: Game;
 
 	constructor(game: Game) {
@@ -16,18 +22,18 @@ export default class GameStartStopButton {
 	private onClick(): void {
 		if (this.game.activated) {
 			this.game.activated = !this.game.activated;
-			this.button.innerText = 'show';
+			this.buttonImage.src = EyeOff;
 			this.game.stop();
 		} else {
 			this.game.activated = !this.game.activated;
-			this.button.innerText = 'hide';
+			this.buttonImage.src = EyeOn;
 			this.game.start();
 			this.game.resize(); // remove listener, wtf how does this work
 		}
 	}
 
 	public start(): void {
-		this.button.innerText = 'show';
+		this.buttonImage.src = EyeOff;
 		this.button.addEventListener('click', this.onClick);
 	}
 

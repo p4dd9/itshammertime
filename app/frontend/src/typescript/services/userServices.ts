1536887e6bcfeee1df526a4698162a9e1350eac4
Patrick Obermueller <patrick.obermueller@willhaben.at>
2022-05-09 21:38:47 +0200

    refactor transactionObject param for sendBits

--- app/frontend/src/typescript/services/userServices.ts
+++ app/frontend/src/typescript/services/userServices.ts
@@ -1,14 +1,18 @@
 import UserDTO from '../../../../backend/src/dto/UserDTO';
+import { TransactionObject } from '../../types/twitch';
 
 export const protocol = 'https';
 export const baseUrl = 'localhost';
 export const port = 3535;
 
-export const sendBits = async (id: string, bit_count: number, sku: string): Promise<boolean> => {
+export const sendBits = async (transactionObject: TransactionObject): Promise<boolean> => {
+	const { userId } = transactionObject;
+	const { cost, sku } = transactionObject.product;
+
 	try {
 		const body = JSON.stringify({
-			id,
-			bit_count,
+			id: userId,
+			bit_count: cost.amount,
 			sku,
 		});
 
@@ -21,7 +25,7 @@ export const sendBits = async (id: string, bit_count: number, sku: string): Prom
 		});
 		return true;
 	} catch (e) {
-		throw new Error(`Couldn't send bit for user: ${id} for product sku: ${sku} `);
+		throw new Error(`Couldn't send bit for user: ${userId} for product sku: ${sku} `);
 	}
 };
 
